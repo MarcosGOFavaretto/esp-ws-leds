9ae233ce2a66e39f17ff3bb27c8fc0a2b6b6fc92
Marcos Gabriel de Oliveira Favaretto <marcosfavaretto.dev@gmail.com>
2024-09-23 00:22:34 +0000

    :rocket:FEATURE: Creates functionality for texts and RGB led

--- esp-ws-leds.ino
+++ esp-ws-leds.ino
@@ -1,31 +1,33 @@
-/*********
-  Rui Santos & Sara Santos - Random Nerd Tutorials
-  Complete project details at https://RandomNerdTutorials.com/esp32-web-server-slider-pwm/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*********/
-// Import required libraries
+// Connection
 #include <WiFi.h>
+
+const char* ssid = "Favaretto - 2.4 GHz";
+const char* password = "09102722";
+//
+
+// Web Server
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+//
 
-// Replace with your network credentials
-const char* ssid = "marcos";
-const char* password = "10092722";
+// LCD
+#include <LiquidCrystal_I2C.h>
 
-const int output = 2;
+#define LCD_COLS 16
+#define LCD_LINES 2
+#define LCD_ADDR 0x27
 
-String sliderValue = "0";
+LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_LINES);
+//
 
-// setting PWM properties
-const int freq = 5000;
-const int ledChannel = 0;
-const int resolution = 8;
+// LED
+#define LED_R 19
+#define LED_G 23
+#define LED_B 18
+//
 
-const char* PARAM_INPUT = "value";
+bool isLedBlinking = false;
 
-// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
 const char index_html[] PROGMEM = R"rawliteral(
@@ -122,6 +124,30 @@ const char index_html[] PROGMEM = R"rawliteral(
         .button:disabled {
             background-color: #a5a5a5;
         }
+
+        #messageInput {
+            margin-top: 10px;
+            padding: 10px;
+            width: 200px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            outline: none;
+        }
+
+        #redButton {
+            background-color: red;
+            color: white;
+        }
+
+        #greenButton {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        #blueButton {
+            background-color: blue;
+            color: white;
+        }
     </style>
 </head>
 
@@ -134,15 +160,36 @@ const char index_html[] PROGMEM = R"rawliteral(
         <span class="slider"></span>
     </label>
 
+    <button id="redButton" class="button" disabled onclick="red()">Vermelho</button>
+    <button id="greenButton" class="button" disabled onclick="green()">Verde</button>
+    <button id="blueButton" class="button" disabled onclick="blue()">Azul</button>
     <button id="blinkButton" class="button" disabled onclick="blinkLED()">Piscar</button>
+
+    <input type="text" id="messageInput" placeholder="Digite sua mensagem" disabled>
+    <button id="sendMessageButton" class="button" disabled onclick="sendMessage()">Enviar Mensagem</button>
 
     <script>
         function toggleButton() {
             var powerSwitch = document.getElementById('powerSwitch');
             var blinkButton = document.getElementById('blinkButton');
+            var redButton = document.getElementById('redButton');
+            var greenButton = document.getElementById('greenButton');
+            var blueButton = document.getElementById('blueButton');
+            var messageInput = document.getElementById('messageInput');
+            var sendMessageButton = document.getElementById('sendMessageButton');
+
             if (powerSwitch.checked) {
                 blinkButton.classList.add('active');
                 blinkButton.disabled = false;
+                redButton.classList.add('active');
+                redButton.disabled = false;
+                greenButton.classList.add('active');
+                greenButton.disabled = false;
+                blueButton.classList.add('active');
+                blueButton.disabled = false;
+                messageInput.disabled = false;
+                sendMessageButton.classList.add('active');
+                sendMessageButton.disabled = false;
 
                 var xhr = new XMLHttpRequest();
                 xhr.open("GET", "/on", true);
@@ -150,6 +197,15 @@ const char index_html[] PROGMEM = R"rawliteral(
             } else {
                 blinkButton.classList.remove('active');
                 blinkButton.disabled = true;
+                redButton.classList.remove('active');
+                redButton.disabled = true;
+                greenButton.classList.remove('active');
+                greenButton.disabled = true;
+                blueButton.classList.remove('active');
+                blueButton.disabled = true;
+                messageInput.disabled = true;
+                sendMessageButton.classList.remove('active');
+                sendMessageButton.disabled = true;
 
                 var xhr = new XMLHttpRequest();
                 xhr.open("GET", "/off", true);
@@ -159,10 +215,38 @@ const char index_html[] PROGMEM = R"rawliteral(
 
         function blinkLED() {
             console.log('LED está piscando');
-            // Enviar solicitação para piscar LED
             var xhr = new XMLHttpRequest();
             xhr.open("GET", "/blink", true);
             xhr.send();
+        }
+
+        function red() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/red", true);
+            xhr.send();
+        }
+
+        function green() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/green", true);
+            xhr.send();
+        }
+
+        function blue() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/blue", true);
+            xhr.send();
+        }
+
+        function sendMessage() {
+            var message = document.getElementById('messageInput').value;
+            console.log('Mensagem enviada:', message);
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "/message", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            var params = "message=" + encodeURIComponent(message);
+            xhr.send(params);
         }
     </script>
 </body>
@@ -170,58 +254,124 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>
 )rawliteral";
 
-// Replaces placeholder with button section in your web page
-String processor(const String& var) {
-  //Serial.println(var);
-  if (var == "SLIDERVALUE") {
-    return sliderValue;
-  }
-  return String();
-}
-
 void setup() {
-  // Serial port for debugging purposes
   Serial.begin(115200);
 
-  // configure LED PWM functionalitites
-  ledcAttachChannel(output, freq, resolution, ledChannel);
-
-  ledcWrite(output, sliderValue.toInt());
-
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
 
-  // Print ESP Local IP Address
   Serial.println(WiFi.localIP());
 
-  // Route for root / web page
+  pinMode(LED_R, OUTPUT);
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
   server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-    request->send_P(200, "text/html", index_html, processor);
+    request->send_P(200, "text/html", index_html);
   });
 
   server.on("/on", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = false;
 
-    ledcWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_R, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_B, HIGH);
+
+    lcd.backlight();
 
     Serial.println("Led on!");
     request->send(200, "text/plain", "OK");
   });
 
   server.on("/off", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = false;
 
-    ledcWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, LOW);
+
+    lcd.noBacklight();
 
     Serial.println("Led off!");
     request->send(200, "text/plain", "OK");
   });
 
-  // Start server
+  server.on("/blink", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = true;
+
+    Serial.println("Led blinking!");
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/red", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = false;
+
+    digitalWrite(LED_R, HIGH);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, LOW);
+
+    Serial.println("Red!");
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/green", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = false;
+
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_B, LOW);
+
+    Serial.println("Green!");
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/blue", HTTP_GET, [](AsyncWebServerRequest* request) {
+    isLedBlinking = false;
+
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, HIGH);
+
+    Serial.println("Blue!");
+    request->send(200, "text/plain", "OK");
+  });
+
+  server.on("/message", HTTP_POST, [](AsyncWebServerRequest* request) {
+    Serial.println("Message!");
+
+    if (request->hasParam("message", true)) {
+      String message = request->getParam("message", true)->value();
+      Serial.println("Text: " + message);
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print(message);
+
+      request->send(200, "text/plain", "OK");
+    } else {
+      request->send(400, "text/plain", "NOT OK");
+    }
+  });
+
   server.begin();
+
+  // LCD
+  lcd.init();
+  //
 }
 
 void loop() {
+  if (isLedBlinking) {
+    digitalWrite(LED_R, LOW);
+    digitalWrite(LED_G, LOW);
+    digitalWrite(LED_B, LOW);
+    delay(1000);
+    digitalWrite(LED_R, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_B, HIGH);
+    delay(1000);
+  }
 }
